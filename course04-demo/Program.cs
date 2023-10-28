7c08fa587efe721e12c691f0a0e320f728f9e1e8
Andrew Wu <chicken@chicken-house.net>
2023-10-29 02:51:12 +0800

    init

---
+++ course04-demo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string first = "Hello";
+string second = "World";
+
+string result1 = string.Format("{0} {1}!", first, second);
+string result2 = $"{first} {second}!";
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
---
+++ demo3/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
