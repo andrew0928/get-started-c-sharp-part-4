449415c76048c5835191c6619310d965e589b8ac
Andrew Wu <chicken@chicken-house.net>
2023-11-08 18:10:57 +0800

    update contents

--- demo4/Program.cs
+++ demo4/Program.cs
@@ -3,8 +3,10 @@ string b = "a";
 string c = new string('a', 1);
 object d = "a";
 object e = c;
+
 Console.WriteLine($"Is string A == B ? {a == b}");  // string.Equals()
 Console.WriteLine($"Is string A == C ? {a == c}");  // string.Equals()
 Console.WriteLine($"Is string A == D ? {a == d}");  // IL: ceq, same object (string literal)
 Console.WriteLine($"Is string A == E ? {a == e}");  // IL: ceq, different object
+
 
